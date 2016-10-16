e9ed68381dfa0d3a6a6dc67a5c3f14ecb0fe7184
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-16 13:44:51 -0500

    Reverted to old tests. Need to include debug flag in functions to test.

--- mausoleum/tests/test_wrapper.py
+++ mausoleum/tests/test_wrapper.py
@@ -18,14 +18,13 @@ def password():
     return 'SUPER_SECURE_PASSWORD'
 
 
-def test_tomb_creation(name, key, password):
-    """Test the creation of a tomb contanier.
+def test_dig_tomb(name):
+    wrapper.dig_tomb(name, 10)
 
-    All three functions are tested here due to a bug in click's
-    CliRunner that does not allow two inputs for the password prompt.
-    """
-    dig = wrapper.dig_tomb(name, 10)
-    if dig == 0:
-        forge = wrapper.forge_tomb(key, password)
-        if forge == 0:
-            wrapper.lock_tomb(name, key, password)
+
+def test_forge_tomb(key, password):
+    wrapper.forge_tomb(key, password)
+
+
+def test_lock_tomb(name, key, password):
+    wrapper.lock_tomb(name, key, password)
