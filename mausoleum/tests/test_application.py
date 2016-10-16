94832dae99955fc11ecb8c49ea7cac6d15408462
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-16 02:12:34 -0500

    Removed unneeded gui tests

--- mausoleum/tests/test_application.py
+++ mausoleum/tests/test_application.py
@@ -20,21 +20,3 @@ def window(qtbot):
 
 def test_window_title(window):
     assert window.windowTitle() == 'Mausoleum'
-
-
-def test_open_file(window, qtbot, mock):
-    """Qtbot clicks on the file sub menu and then navigates to the Open File item. Mock creates
-    an object to be passed to the QFileDialog."""
-    qtbot.mouseClick(window.file_sub_menu, Qt.LeftButton)
-    qtbot.keyClick(window.file_sub_menu, Qt.Key_Down)
-    mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('', ''))
-    qtbot.keyClick(window.file_sub_menu, Qt.Key_Enter)
-
-
-def test_about_dialog(window, qtbot, mock):
-    """Qtbot clicks on the help sub menu and then navigates to the About item. Mock creates
-    a QDialog object to be used for the test."""
-    qtbot.mouseClick(window.help_sub_menu, Qt.LeftButton)
-    qtbot.keyClick(window.help_sub_menu, Qt.Key_Down)
-    mock.patch.object(QDialog, 'exec_', return_value='accept')
-    qtbot.keyClick(window.help_sub_menu, Qt.Key_Enter)
