a83de99ce2b902b1e30a6303646d7d76f8b2e179
mandeepbhutani <mandeep@users.noreply.github.com>
2016-10-16 21:57:46 -0500

    New minor release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mausoleum',
-    version='0.0.1',
+    version='0.2.0',
     description='A Python wrapper and GUI application for Tomb',
     author='Mandeep',
     author_email='info@mandeep.xyz',
