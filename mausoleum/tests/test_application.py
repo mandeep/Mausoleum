d497b8581d218872d8cd6d5c5998a16ad85d2fd6
mandeep <mandeep@users.noreply.github.com>
2016-11-09 21:34:09 -0600

    Added docstrings to all functions

--- mausoleum/tests/test_application.py
+++ mausoleum/tests/test_application.py
@@ -7,8 +7,11 @@ from mausoleum import application
 
 @pytest.fixture
 def window(qtbot):
-    """Window is used as a pytest fixture that allows passing it to test functions. Qtbot
-    uses qApp to open a new main window."""
+    """Create an instance of Mausoleum to pass to test functions.
+
+    window is used as a pytest fixture that allows passing it to test functions. Qtbot
+    uses qApp to open a new main window for each test function.
+    """
     new_window = application.Mausoleum()
     qtbot.add_widget(new_window)
     new_window.show()
@@ -17,24 +20,29 @@ def window(qtbot):
 
 @pytest.fixture
 def name():
+    """Use test1.tomb as the tomb name to pass to test functions."""
     return 'test1.tomb'
 
 
 @pytest.fixture
 def key():
+    """Use test1.tomb.key as the tomb key name to pass to test functions."""
     return 'test1.tomb.key'
 
 
 @pytest.fixture
 def password():
+    """Use test_password as the tomb password to pass to test functions."""
     return 'test_password'
 
 
 def test_window_title(window):
+    """Check the window title of the main window."""
     assert window.windowTitle() == 'Mausoleum'
 
 
 def test_create_page_failure(window, qtbot):
+    """Test tomb creation with key passwords that don't match."""
     button = window.create_page.create_button
     window.create_page.key_password.setText('1')
     window.create_page.confirm_password.setText('2')
@@ -43,6 +51,7 @@ def test_create_page_failure(window, qtbot):
 
 
 def test_create_page(window, qtbot, name, key, password):
+    """Test tomb creation and tomb opening."""
     window.create_page.tomb_name.setText(name)
     window.create_page.key_name.setText(key)
     window.create_page.key_password.setText(password)
@@ -55,12 +64,14 @@ def test_create_page(window, qtbot, name, key, password):
 
 
 def test_close_page_close(window, qtbot):
+    """Test the Close All Tombs button on the Close tab."""
     window.pages.setCurrentIndex(2)
     button = window.close_page.close_all_button
     qtbot.mouseClick(button, Qt.LeftButton)
 
 
 def test_open_page(window, qtbot, name, key, password):
+    """Test tomb opening on the Open tab."""
     window.pages.setCurrentIndex(1)
     window.open_page.tomb_path.setText(name)
     window.open_page.key_path.setText(key)
@@ -71,6 +82,7 @@ def test_open_page(window, qtbot, name, key, password):
 
 
 def test_close_page_force_close(window, qtbot):
+    """Test the Force Close Tombs on the Close tab."""
     window.pages.setCurrentIndex(2)
     button = window.close_page.force_close_button
     qtbot.mouseClick(button, Qt.LeftButton)
