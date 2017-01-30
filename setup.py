19ddcdc7a48e875042f13b28951be0c73e7ed3a8
mandeep <mandeep@users.noreply.github.com>
2017-01-30 10:05:02 -0600

    Updated Tomb from 2.2 to 2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mausoleum',
-    version='0.4.2',
+    version='0.4.3',
     description='A Python GUI, CLI, and wrapper for Tomb',
     author='Mandeep',
     author_email='info@mandeep.xyz',
