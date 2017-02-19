a6c84407cc0100309405d16f55350ef8e66ddc12
mandeep <mandeep@users.noreply.github.com>
2017-02-19 00:10:42 -0600

    0.7.0 release with CLI commands for engrave, bury, and exhume

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='mausoleum',
-    version='0.6.1',
+    version='0.7.0',
     description='A Python GUI, CLI, and wrapper for Tomb',
     author='Mandeep',
     author_email='info@mandeep.xyz',
